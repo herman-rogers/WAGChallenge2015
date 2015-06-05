50e39d42227fbce99c338bfaf705fb98247c98c4
Boomer <herman.rogers@gmail.com>
2015-06-05 22:07:18 +0100

    CONTROLLER: Adding basic character controller, observer pattern, command pattern, and a basic camera follow -- just the basic 2d tools

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform followTarget;
+	
+	void FixedUpdate () {
+        Vector3 targetsPosition = new Vector3( followTarget.position.x, 
+                                               followTarget.position.y, 
+                                               transform.position.z );
+        transform.position = targetsPosition;
+	}
+}
---
+++ Assets/Scripts/PlayerScripts/MainCharacterController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCharacterController : MonoBehaviour {
+
+    public float maxSpeed = 10.0f;
+    private Rigidbody2D characterRigidbody;
+    private Animator characterAnimator;
+    private bool facingRight = true;
+
+    private void Start( ) {
+        characterRigidbody = GetComponent<Rigidbody2D>( );
+        characterAnimator = GetComponent<Animator>( );
+    }
+
+    private void FixedUpdate( ) {
+        float move = Input.GetAxis("Horizontal");
+        characterAnimator.SetFloat( "Speed", Mathf.Abs( move ) );
+
+        characterRigidbody.velocity = new Vector2( move * maxSpeed, characterRigidbody.velocity.y );
+        ChangeFacingDirection( move );
+    }
+
+    private void ChangeFacingDirection( float movement ) {
+        if ( movement > 0 && !facingRight ) {
+            InvertDirection( );
+        } else if ( movement < 0 && facingRight ) {
+            InvertDirection( );
+        }
+    }
+
+    private void InvertDirection( ) {
+        facingRight = !facingRight;
+        Vector3 facingDirection = transform.localScale;
+        facingDirection.x *= -1;
+        transform.localScale = facingDirection;
+    }
+}
