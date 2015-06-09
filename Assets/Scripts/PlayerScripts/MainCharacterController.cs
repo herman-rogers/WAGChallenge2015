d17f3e44f4a9186c58039480f500e65e94a3f1ae
Boomer <herman.rogers@gmail.com>
2015-06-09 20:55:41 +0100

    Adding in Jump, jump animations, rough speed tools, extended level,

--- Assets/Scripts/PlayerScripts/MainCharacterController.cs
+++ Assets/Scripts/PlayerScripts/MainCharacterController.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class MainCharacterController : MonoBehaviour {
 
     public float maxSpeed = 10.0f;
+    public float acceleration = 1.0f;
+    public float jumpForce = 400;
+    public bool isGrounded = false;
     private Rigidbody2D characterRigidbody;
     private Animator characterAnimator;
+    private float accelerationMultiplier = 0.0f;
     private bool facingRight = true;
 
     private void Start( ) {
@@ -14,11 +18,36 @@ public class MainCharacterController : MonoBehaviour {
     }
 
     private void FixedUpdate( ) {
-        float move = Input.GetAxis("Horizontal");
-        characterAnimator.SetFloat( "Speed", Mathf.Abs( move ) );
+        float move = Input.GetAxis( "Horizontal" );
+        if ( isGrounded ) {
+            MoveCharacter( move );
+        }
+    }
 
-        characterRigidbody.velocity = new Vector2( move * maxSpeed, characterRigidbody.velocity.y );
+    private void Update( ) {
+        if ( isGrounded && Input.GetKeyDown( KeyCode.Space ) ) {
+            characterAnimator.SetBool( "Ground", false );
+            GetComponent<Rigidbody2D>( ).AddForce( new Vector2( 0, jumpForce ) );
+        } else {
+            characterAnimator.SetBool( "Ground", isGrounded );
+        }
+    }
+
+    private void MoveCharacter( float move ) {
+        float normalizeMove = Mathf.Abs( move );
+        characterAnimator.SetFloat( "Speed", normalizeMove );
+        characterRigidbody.velocity = new Vector2( ( move * maxSpeed ),
+                                                     characterRigidbody.velocity.y ); //accelerationMultiplier
+        ChangeAcceleration( normalizeMove );
         ChangeFacingDirection( move );
+    }
+
+    private void ChangeAcceleration( float movement ) {
+        if ( movement > 0 && movement < maxSpeed ) {
+            accelerationMultiplier += ( Time.deltaTime * ( acceleration ) );
+        } else if ( movement == 0 ) {
+            accelerationMultiplier = 0;
+        }
     }
 
     private void ChangeFacingDirection( float movement ) {
@@ -31,6 +60,7 @@ public class MainCharacterController : MonoBehaviour {
 
     private void InvertDirection( ) {
         facingRight = !facingRight;
+        accelerationMultiplier = 0;
         Vector3 facingDirection = transform.localScale;
         facingDirection.x *= -1;
         transform.localScale = facingDirection;
---
+++ Assets/Scripts/PlayerScripts/PlayerGroundCheck.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGroundCheck : MonoBehaviour {
+
+    public LayerMask whatIsGround;
+    private MainCharacterController playerController;
+
+    private void Start( ) {
+        playerController = GetComponentInParent<MainCharacterController>( );
+    }
+
+    private void FixedUpdate( ) {
+        playerController.isGrounded = Physics2D.OverlapCircle( transform.position, 0.2f, whatIsGround );
+    }
+}
